2c77294612dc5f3fe95cddf7d74509134d2b468c
lululombard <lululombard@gmail.com>
2020-11-21 21:28:11 -0500

    Working SpigotMC downloads

--- download/manager.py
+++ download/manager.py
@@ -1,4 +1,8 @@
 
+import traceback
+
+from tqdm import tqdm
+
 from download.sources.spigotmc_source import SpigotMcSource
 
 
@@ -7,3 +11,25 @@ class DownloadManager():
 
     def __init__(self):
         self.spigotmc_source = SpigotMcSource()
+
+    def download(self, source, name, url, type, **kwargs):
+        source_manager = None
+
+        try:
+            source_manager = getattr(self, '{}_source'.format(source))
+        except AttributeError:
+            pass
+
+        if not source_manager:
+            raise ValueError('Source {} is not supported'.format(source))
+
+        downloaded_binary = source_manager.download_element(url, **kwargs)
+
+        print('{} type {} size {}'.format(name, type, len(downloaded_binary)))
+
+    def download_resources(self, resources):
+        for resource in tqdm(resources):
+            try:
+                self.download(**resource)
+            except:
+                traceback.print_exc()
--- redcraft_plugin_updater.py
+++ redcraft_plugin_updater.py
@@ -1,26 +1,15 @@
-import os
-
-from tqdm import tqdm
+import json
 from dotenv import load_dotenv
 
-from spigotmc_client import SpigotMcClient
+from download.manager import DownloadManager
 
 
 if __name__ == '__main__':
     load_dotenv()
 
-    # Get settings from env or the .env file
-    login = os.environ.get('SPIGOTMC_LOGIN')
-    password = os.environ.get('SPIGOTMC_PASSWORD')
-    output_folder = os.environ.get('OUTPUT_FOLDER', 'plugins')
-    cloudproxy_url = os.environ.get('CLOUDPROXY_URL', 'http://localhost:8191/v1')
+    with open('resources_to_download.json') as resources_file:
+        resources = json.load(resources_file)
 
-    client = SpigotMcClient()
+        download_manager = DownloadManager()
 
-    client.log_in(login, password, cloudproxy_url=cloudproxy_url)
-
-    watched_plugins = client.get_watched_plugins()
-
-    # Download every plugin we watch
-    for plugin_data in tqdm(watched_plugins.values(), desc='Downloading plugins'):
-        new_plugin_data = client.download_plugin(plugin_data, output_folder)
+        download_manager.download_resources(resources)
